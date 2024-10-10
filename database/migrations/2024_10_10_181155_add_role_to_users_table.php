ad5689ae93e7c1cdc8fdcdf09d74a31e178fd132
Miguel Ramos <return.infox@gmail.com>
2024-10-10 15:55:38 -0300

    migraciones añadiendo role

--- database/migrations/2024_10_10_181155_add_role_to_users_table.php
+++ database/migrations/2024_10_10_181155_add_role_to_users_table.php
@@ -9,20 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('role')->default('User');  // Añadir la columna 'role'
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role');  // Eliminar la columna en caso de rollback
         });
     }
 };
